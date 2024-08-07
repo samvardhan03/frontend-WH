752610cac4dcfb0ba354e5f2648e491f7279d403
lighting129 <devrajajoo@gmail.com>
2024-08-07 15:35:20 +0530

    testing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import "./HomePage.css";
 const HomePage = () => {
-    return (
-        <div>
-            <h1>DEVRAJ PLEASE MAKE THIS LOOK GOOD(SHOULD BE RESPONSIVE TOO)</h1>
-        </div>
-    );
+  return (
+    <div className="home-page">
+      <h1>DEVRAJ PLEASE MAKE THIS LOOK GOOD(SHOULD BE RESPONSIVE TOO)</h1>
+    </div>
+  );
 };
 
 export default HomePage;
