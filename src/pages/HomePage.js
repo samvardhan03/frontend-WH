a17bbdbeaed8f35284155104379ec4d6b3a72cd3
lighting129 <devrajajoo@gmail.com>
2024-08-07 16:01:41 +0530

    t

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,11 @@ import "./HomePage.css";
 const HomePage = () => {
   return (
     <div className="home-page">
-      <div className="section">
+      <div className="card">
         <h1>Section 1</h1>
         <p>This is the first section.</p>
       </div>
-      <div className="section">
+      <div className="card">
         <h1>Section 2</h1>
         <ul>
           <li>Item 1</li>
@@ -15,9 +15,9 @@ const HomePage = () => {
           <li>Item 3</li>
         </ul>
       </div>
-      <div className="section">
+      <div className="card">
         <h1>Section 3</h1>
-        <img src="image.jpg" alt="An image" />
+        <p>here the data</p>
       </div>
     </div>
   );
