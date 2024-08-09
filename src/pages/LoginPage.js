09ffa7c558296dce39fe9a1cb7e4659741d7dd21
lighting129 <devrajajoo@gmail.com>
2024-08-09 05:31:46 +0530

    login3

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -18,77 +18,43 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="overlay">
-      <link rel="stylesheet" href="LoginPage.css" />
-      {/* LOGN IN FORM by Omar Dsoky */}
-      <form>
-        {/*   con = Container  for items in the form*/}
-        <div className="con">
-          {/*     Start  header Content  */}
-          <header className="head-form">
-            <h2>Log In</h2>
-            {/*     A welcome message or an explanation of the login form */}
-            <p>login here using your username and password</p>
-          </header>
-          {/*     End  header Content  */}
-          <br />
-          <div className="field-set">
-            {/*   user name */}
-            <span className="input-item">
-              <i className="fa fa-user-circle" />
-            </span>
-            {/*   user name Input*/}
-            <input
-              className="form-input"
-              id="txt-input"
-              type="text"
-              placeholder="@UserName"
-              required
-            />
-            <br />
-            {/*   Password */}
-            <span className="input-item">
-              <i className="fa fa-key" />
-            </span>
-            {/*   Password Input*/}
-            <input
-              className="form-input"
-              type="password"
-              placeholder="Password"
-              id="pwd"
-              name="password"
-              required
-            />
-            {/*      Show/hide password  */}
-            <span>
-              <i
-                className="fa fa-eye"
-                aria-hidden="true"
-                type="button"
-                id="eye"
+    <div>
+      <div className="bold-line" />
+      <div className="container">
+        <div className="window">
+          <div className="overlay" />
+          <div className="content">
+            <div className="welcome">Hello There!</div>
+            <div className="subtitle">
+              We're almost done. Before using our services you need to create an
+              account.
+            </div>
+            <div className="input-fields">
+              <input
+                type="text"
+                placeholder="Username"
+                className="input-line full-width"
               />
-            </span>
-            <br />
-            {/*        buttons */}
-            {/*      button LogIn */}
-            <button className="log-in"> Log In </button>
+              <input
+                type="email"
+                placeholder="Email"
+                className="input-line full-width"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="input-line full-width"
+              />
+            </div>
+            <div className="spacing">
+              or continue with <span className="highlight">Facebook</span>
+            </div>
+            <div>
+              <button className="ghost-round full-width">Create Account</button>
+            </div>
           </div>
-          {/*   other buttons */}
-          <div className="other">
-            {/*      Forgot Password button*/}
-            <button className="btn submits frgt-pass">Forgot Password</button>
-            {/*     Sign Up button */}
-            <button className="btn submits sign-up">
-              Sign Up
-              {/*         Sign Up font icon */}
-              <i className="fa fa-user-plus" aria-hidden="true" />
-            </button>
-            {/*      End Other the Division */}
-          </div>
-          {/*   End Conrainer  */}
         </div>
-        {/* End Form */}
-      </form>
+      </div>
     </div>
   );
 };
